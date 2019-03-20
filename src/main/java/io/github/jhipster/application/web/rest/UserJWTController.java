0c0c79e5f59c5b5e5e168ae1d2fa4cf8455b3eac
Horrell <charles.horrell@dxc.com>
2019-03-20 09:59:45 +0000

    Change cast back to authentication manager

--- src/main/java/io/github/jhipster/application/web/rest/UserJWTController.java
+++ src/main/java/io/github/jhipster/application/web/rest/UserJWTController.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,11 +27,11 @@ public class UserJWTController {
 
     private final TokenProvider tokenProvider;
 
-    private final CustomAuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
 
-    public UserJWTController(TokenProvider tokenProvider, CustomAuthenticationManager authenticationManager) {
+    public UserJWTController(TokenProvider tokenProvider, AuthenticationManager authenticationManager) {
         this.tokenProvider = tokenProvider;
-        this.authenticationManager = authenticationManager;
+        this.authenticationManager = (AuthenticationManager)authenticationManager;
     }
 
     @PostMapping("/authenticate")
